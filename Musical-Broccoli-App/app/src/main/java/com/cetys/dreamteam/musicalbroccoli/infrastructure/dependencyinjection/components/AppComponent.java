7286288e4a924d9aae8eeff3815d125bf9309b35
J. Pichardo <pichardo.software@gmail.com>
2016-11-11 23:51:47 -0800

    User Subcomponent

--- Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/infrastructure/dependencyinjection/components/AppComponent.java
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/infrastructure/dependencyinjection/components/AppComponent.java
@@ -1,5 +1,9 @@
 package com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.components;
 
+import com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.modules.AppModule;
+import com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.modules.NetModule;
+import com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.modules.UserModule;
+
 import javax.inject.Singleton;
 
 import dagger.Component;
@@ -8,6 +12,7 @@ import dagger.Component;
  * @Author J. Pichardo on 11/11/2016.
  */
 @Singleton
-@Component
+@Component(modules = { AppModule.class, NetModule.class})
 public interface AppComponent {
+    UserSubcomponent plus( UserModule userModule);
 }
