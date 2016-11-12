89500e5f7309f5f6756d36cdaf157501d5140d6b
J. Pichardo <pichardo.software@gmail.com>
2016-11-11 23:29:09 -0800

    Modules

---
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/infrastructure/dependencyinjection/components/AppComponent.java
@@ -0,0 +1,10 @@
+package com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.components;
+
+import dagger.Component;
+
+/**
+ * @Author J. Pichardo on 11/11/2016.
+ */
+@Component
+public interface AppComponent {
+}
