5e9ea13864186f91b500c810723d2ed75ac3c85f
J. Pichardo <pichardo.software@gmail.com>
2016-11-11 23:16:01 -0800

    Base Stuff

---
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/networking/request/BaseRequest.java
@@ -0,0 +1,10 @@
+package com.cetys.dreamteam.musicalbroccoli.networking.request;
+
+/**
+ * @Author J. Pichardo on 11/11/2016.
+ */
+
+public class BaseRequest< TModel > {
+
+    private TModel data;
+}
---
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/presentation/connectors/BaseConnector.java
@@ -0,0 +1,8 @@
+package com.cetys.dreamteam.musicalbroccoli.presentation.connectors;
+
+/**
+ * @Author J. Pichardo on 11/11/2016.
+ */
+
+public abstract class BaseConnector {
+}
