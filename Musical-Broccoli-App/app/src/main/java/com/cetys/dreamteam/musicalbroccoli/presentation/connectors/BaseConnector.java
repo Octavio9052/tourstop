48002d71cf5cd5c4e9deb342d59a2fc8fd2b494c
J. Pichardo <pichardo.software@gmail.com>
2016-11-11 23:17:45 -0800

    Base Connector

--- Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/presentation/connectors/BaseConnector.java
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/presentation/connectors/BaseConnector.java
@@ -4,5 +4,5 @@ package com.cetys.dreamteam.musicalbroccoli.presentation.connectors;
  * @Author J. Pichardo on 11/11/2016.
  */
 
-public abstract class BaseConnector {
+public abstract class BaseConnector<TService, TDto, TModel> {
 }
