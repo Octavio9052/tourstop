75cda4ea5c5c28f9be7f08416f114251671b57d6
J. Pichardo <pichardo.software@gmail.com>
2016-11-11 23:47:05 -0800

    NetModule

--- Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/infrastructure/dependencyinjection/components/AppComponent.java
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/infrastructure/dependencyinjection/components/AppComponent.java
@@ -1,10 +1,13 @@
 package com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.components;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
 /**
  * @Author J. Pichardo on 11/11/2016.
  */
+@Singleton
 @Component
 public interface AppComponent {
 }
--- Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/infrastructure/dependencyinjection/modules/AppModule.java
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/infrastructure/dependencyinjection/modules/AppModule.java
@@ -1,10 +1,27 @@
 package com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.modules;
 
+import android.app.Application;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 /**
  * @Author J. Pichardo on 11/11/2016.
  */
 @Module
 public class AppModule {
+
+    private Application application;
+
+    public AppModule( Application application ) {
+        this.application = application;
+    }
+
+    @Provides
+    @Singleton
+    Application providesApplication() {
+        return this.application;
+    }
 }
