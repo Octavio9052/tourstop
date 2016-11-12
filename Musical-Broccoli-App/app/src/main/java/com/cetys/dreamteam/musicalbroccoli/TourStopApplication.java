4242429f3670d5e719553bdfcdbfc95f0e7e9b56
J. Pichardo <pichardo.software@gmail.com>
2016-11-11 23:58:34 -0800

    Applicatino

---
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/TourStopApplication.java
@@ -0,0 +1,46 @@
+package com.cetys.dreamteam.musicalbroccoli;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.components.AppComponent;
+import com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.components.DaggerAppComponent;
+import com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.components.UserSubcomponent;
+
+/**
+ * @Author J. Pichardo on 11/11/2016.
+ */
+
+public class TourStopApplication extends Application {
+
+    private AppComponent appComponent;
+    private UserSubcomponent userSubcomponent;
+
+    public static TourStopApplication get( Context context ) {
+        return ( TourStopApplication ) context.getApplicationContext();
+    }
+
+    public void createUserComponent() {
+
+    }
+
+    private void bootstrap() {
+        this.appComponent = DaggerAppComponent.builder().build();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        bootstrap();
+
+    }
+
+    public AppComponent getAppComponent() {
+        return appComponent;
+    }
+
+    public UserSubcomponent getUserSubcomponent() {
+        return userSubcomponent;
+    }
+}
