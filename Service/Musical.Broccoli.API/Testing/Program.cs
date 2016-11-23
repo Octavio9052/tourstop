eafbe34900bfaad63866a72592524eb1d7a3051f
alexc <alejandro.cruz@cetys.edu.mx>
2016-11-22 23:08:48 -0800

    Test

--- Service/Musical.Broccoli.API/Testing/Program.cs
+++ Service/Musical.Broccoli.API/Testing/Program.cs
@@ -9,6 +9,7 @@ using DataAccessLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using Common.AppSettings;
 using MySQL.Data.EntityFrameworkCore.Extensions;
+using Common.Enums;
 //using MySQL.Data.EntityFrameworkCore.Extensions;
 
 namespace Testing
@@ -17,25 +18,7 @@ namespace Testing
     {
         public static void Main(string[] args)
         {
-            User user = new User
-            {
-                Address = new Address
-                {
-                    Street1 = "#1",
-                    City = "CityTest",
-                    Name = "Test1",
-                    PostalCode = 22502,
-                    State = "BAJA",
-                    CountryCode = Common.Enums.CountryCode.MX
-                },
-                FirstName = "TestName1",
-                LastName = "LastName2",
-                Email = "test_unitesting@test.com",
-                Phone = "64617850353",
-                Password = "123456",
-                LanguageCode = Common.Enums.LanguageCode.ES,
-                UserType = Common.Enums.UserType.Promotor
-            };
+            
             var connectionString = @"server=localhost;userid=root;pwd=3585;port=3306;database=DBTourStop2;";
             Common.AppSettings.AppSettings.ConnectionString = connectionString;
             var optionsBuilder = new DbContextOptionsBuilder<TourStopContext>();
@@ -46,23 +29,51 @@ namespace Testing
             var userRepo = new UserRepository(new TourStopContext(optionsBuilder.Options));
             var users = new List<User>();
             Console.WriteLine("Adding User");
-            users.Add(user);
+            User user1 = getNewUser("1",UserType.Promotor);
+            User user2 = getNewUser("2",UserType.User);
+            users.Add(user1);
+            users.Add(user2);
             userRepo.Add(users);
-            Console.WriteLine("User added with Id="+users.First().Id);
+            Console.WriteLine("User added with Id=" + users.First().Id);
             var newusers = userRepo.GetQueryable().Where(x => x.Email == "test_unitesting@test.com").ToList();
-            var user1 = newusers.First();
-            Console.WriteLine("Updating User Firstname: "+user1.FirstName);
+            var useredited = newusers.First();
+            Console.WriteLine("Updating User Firstname: " + user1.FirstName);
             user1.FirstName = "works";
             userRepo.Update(newusers);
             Console.WriteLine("Updated User Firstname: " + newusers.First().FirstName);
+
+
 
 
             //REMOVE
             userRepo.Remove(newusers);
             Console.WriteLine("User Removed");
             Console.ReadLine();
-        
-            
+
+
+        }
+
+        private static User getNewUser(string id, UserType usertype)
+        {
+            return new User
+            {
+                Address = new Address
+                {
+                    Street1 = "CalleTest",
+                    City = "CityTest",
+                    Name = "Test1",
+                    PostalCode = 22502,
+                    State = "BAJA",
+                    CountryCode = Common.Enums.CountryCode.MX
+                },
+                FirstName = String.Format("TestName{0}", id),
+                LastName = String.Format("LastName{0}", id),
+                Email =String.Format( "test_unitesting{0}@test.com",id),
+                Phone = "64617850353",
+                Password = "123456",
+                LanguageCode = Common.Enums.LanguageCode.ES,
+                UserType = Common.Enums.UserType.Promotor
+            };
         }
     }
 }
