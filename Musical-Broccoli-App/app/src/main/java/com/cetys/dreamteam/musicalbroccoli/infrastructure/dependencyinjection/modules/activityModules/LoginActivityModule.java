2c84802bb22225109d5e84c892b516b3938a3d4f
Octavio Armenta <armenta.octavio@outlook.com>
2016-11-23 13:46:44 -0800

    Temp

--- Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/infrastructure/dependencyinjection/modules/activityModules/LoginActivityModule.java
+++
@@ -1,8 +0,0 @@
-package com.cetys.dreamteam.musicalbroccoli.infrastructure.dependencyinjection.modules.activityModules;
-
-/**
- * @Author J. Pichardo on 11/21/2016.
- */
-
-public class LoginActivityModule {
-}
