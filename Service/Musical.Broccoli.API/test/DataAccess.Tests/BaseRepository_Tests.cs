0dbfed6f8eff7d7d78ae7232d60ab44df389c64a
J. Pichardo <pichardo.software@gmail.com>
2016-12-01 19:04:39 -0800

    Repo Test

--- Service/Musical.Broccoli.API/test/DataAccess.Tests/BaseRepository_Tests.cs
+++ Service/Musical.Broccoli.API/test/DataAccess.Tests/BaseRepository_Tests.cs
@@ -10,17 +10,15 @@ namespace DataAccess.Tests
 {
     public class BaseRepository_Tests
     {
-
         [Fact]
         public void AddOrUpdate_NewUser_UserAdded()
         {
-
             var options = new DbContextOptionsBuilder<TourStopContext>()
-                .UseInMemoryDatabase( "Add_Test_TourStop_Db" ).Options;
+                .UseInMemoryDatabase("Add_Test_TourStop_Db").Options;
 
-            using (var context = new TourStopContext( options ))
+            using (var context = new TourStopContext(options))
             {
-                var repository = new UserRepository( context );
+                var repository = new UserRepository(context);
                 var user = new User
                 {
                     FirstName = "FirstName",
@@ -33,43 +31,42 @@ namespace DataAccess.Tests
                     UserType = UserType.Promoter
                 };
 
-                repository.AddOrUpdate( user );
+                repository.AddOrUpdate(user);
                 repository.SaveChanges();
             }
-            using (var context = new TourStopContext( options ))
+            using (var context = new TourStopContext(options))
             {
-                Assert.Equal( 1, context.Users.Count() );
-                Assert.Equal( "fooAdd@bar.com", context.Users.Single().Email );
+                Assert.Equal(1, context.Users.Count());
+                Assert.Equal("fooAdd@bar.com", context.Users.Single().Email);
             }
-
         }
 
         [Fact]
         public void AddOrUpdate_User_UserChanged()
         {
             var options = new DbContextOptionsBuilder<TourStopContext>()
-                .UseInMemoryDatabase( "Update_Test_TourStop_Db" ).Options;
+                .UseInMemoryDatabase("Update_Test_TourStop_Db").Options;
 
-            using (var context = new TourStopContext( options ))
+            using (var context = new TourStopContext(options))
             {
-                context.Users.Add( new User { Email = "fooUpdate@bar.com", FirstName = "Update" } );
+                context.Users.Add(new User {Email = "fooUpdate@bar.com", FirstName = "Update"});
                 context.SaveChanges();
             }
 
-            using (var context = new TourStopContext( options ))
+            using (var context = new TourStopContext(options))
             {
-                var repository = new UserRepository( context );
+                var repository = new UserRepository(context);
                 var user = repository.GetQueryable().Single();
 
                 user.FirstName = "Updated";
-                repository.AddOrUpdate( user );
+                repository.AddOrUpdate(user);
                 repository.SaveChanges();
             }
 
-            using (var context = new TourStopContext( options ))
+            using (var context = new TourStopContext(options))
             {
-                Assert.Equal( 1, context.Users.Count() );
-                Assert.Equal( "Updated", context.Users.Single().FirstName );
+                Assert.Equal(1, context.Users.Count());
+                Assert.Equal("Updated", context.Users.Single().FirstName);
             }
         }
 
@@ -77,31 +74,31 @@ namespace DataAccess.Tests
         public void Delete_User_UserDeleted()
         {
             var options = new DbContextOptionsBuilder<TourStopContext>()
-                .UseInMemoryDatabase( "Delete_Test_TourStop_Db" ).Options;
+                .UseInMemoryDatabase("Delete_Test_TourStop_Db").Options;
 
-            using (var context = new TourStopContext( options ))
+            using (var context = new TourStopContext(options))
             {
-                context.Users.Add( new User { Email = "fooDelete1@bar.com" } );
-                context.Users.Add( new User { Email = "fooDelete2@bar.com" } );
-                context.Users.Add( new User { Email = "fooDelete3@bar.com" } );
+                context.Users.Add(new User {Email = "fooDelete1@bar.com"});
+                context.Users.Add(new User {Email = "fooDelete2@bar.com"});
+                context.Users.Add(new User {Email = "fooDelete3@bar.com"});
 
                 context.SaveChanges();
             }
 
-            using (var context = new TourStopContext( options ))
+            using (var context = new TourStopContext(options))
             {
-                var repository = new UserRepository( context );
+                var repository = new UserRepository(context);
 
-                var user = repository.GetQueryable().Where( x => x.Email == "fooDelete1@bar.com" ).Single();
+                var user = repository.GetQueryable().Where(x => x.Email == "fooDelete1@bar.com").Single();
 
-                repository.Remove( user );
+                repository.Remove(user);
                 repository.SaveChanges();
             }
 
-            using (var context = new TourStopContext( options ))
+            using (var context = new TourStopContext(options))
             {
-                Assert.Equal( 2, context.Users.Count() );
-                Assert.All( context.Users, x => Assert.NotEqual( "fooDelete1@bar.com", x.Email ) );
+                Assert.Equal(2, context.Users.Count());
+                Assert.All(context.Users, x => Assert.NotEqual("fooDelete1@bar.com", x.Email));
             }
         }
 
@@ -109,23 +106,21 @@ namespace DataAccess.Tests
         public void GetAll_User_TwoRegistries()
         {
             var options = new DbContextOptionsBuilder<TourStopContext>()
-                .UseInMemoryDatabase( "GetAll_Test_TourStop_Db" ).Options;
+                .UseInMemoryDatabase("GetAll_Test_TourStop_Db").Options;
 
-            using (var context = new TourStopContext( options ))
+            using (var context = new TourStopContext(options))
             {
-                context.Add( new User { Email = "fooGetAll1@bar.com" } );
-                context.Add( new User { Email = "fooGetAll2@bar.com" } );
+                context.Add(new User {Email = "fooGetAll1@bar.com"});
+                context.Add(new User {Email = "fooGetAll2@bar.com"});
 
                 context.SaveChanges();
             }
 
-            using (var context = new TourStopContext( options ))
+            using (var context = new TourStopContext(options))
             {
-                var repository = new UserRepository( context );
-                Assert.Equal( 2, repository.GetAll().Count );
+                var repository = new UserRepository(context);
+                Assert.Equal(2, repository.GetAll().Count);
             }
-
         }
-
     }
 }
