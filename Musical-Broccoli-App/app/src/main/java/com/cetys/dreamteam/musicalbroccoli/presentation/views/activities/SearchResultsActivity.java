541fc1a3bc4e0761c9b42bb056aee43652484e0e
Paulina A. Meza Paz <paulina.meza96@gmail.com>
2016-12-03 15:21:52 -0800

    message

--- Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/presentation/views/activities/SearchResultsActivity.java
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/presentation/views/activities/SearchResultsActivity.java
@@ -3,6 +3,8 @@ package com.cetys.dreamteam.musicalbroccoli.presentation.views.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cetys.dreamteam.musicalbroccoli.R;
+
 public class SearchResultsActivity extends AppCompatActivity {
 
     @Override
