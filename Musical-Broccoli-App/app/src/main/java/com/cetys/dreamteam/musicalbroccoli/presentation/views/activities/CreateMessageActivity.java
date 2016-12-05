1c41a02292cd38fa4c17a8b70c327db32c63466b
Paulina A. Meza Paz <paulina.meza96@gmail.com>
2016-12-05 15:40:34 -0800

    CreateMessage View

---
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/presentation/views/activities/CreateMessageActivity.java
@@ -0,0 +1,16 @@
+package com.cetys.dreamteam.musicalbroccoli.presentation.views.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cetys.dreamteam.musicalbroccoli.R;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(
+                R.layout.create_message_activity);
+    }
+}
