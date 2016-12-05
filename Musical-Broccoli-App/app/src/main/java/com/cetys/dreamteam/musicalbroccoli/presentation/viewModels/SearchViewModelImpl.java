9453b674c827db3c4b46828634477bdd9823afe7
Octavio Armenta <armenta.octavio@outlook.com>
2016-12-04 16:24:51 -0800

    Access to all current activities: GRANTED

--- Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/presentation/viewModels/SearchViewModelImpl.java
+++ Musical-Broccoli-App/app/src/main/java/com/cetys/dreamteam/musicalbroccoli/presentation/viewModels/SearchViewModelImpl.java
@@ -1,9 +1,12 @@
 package com.cetys.dreamteam.musicalbroccoli.presentation.viewModels;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 
 import com.cetys.dreamteam.musicalbroccoli.presentation.models.TourModel;
 import com.cetys.dreamteam.musicalbroccoli.presentation.viewModels.contracts.SearchViewModel;
+import com.cetys.dreamteam.musicalbroccoli.presentation.views.activities.SearchResultsActivity;
 
 /**
  * Created by Octavio on 2016/11/16.
@@ -24,5 +27,11 @@ public class SearchViewModelImpl extends BaseViewModel implements SearchViewMode
     @Override
     public void setTour(TourModel tour) {
         this.tour = tour;
+    }
+
+    @Override
+    public void onSearchClick(View view) {
+        Intent intent = new Intent(context, SearchResultsActivity.class);
+        context.startActivity(intent);
     }
 }
